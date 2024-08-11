12dd57756a34ac49dddab7352805ca7d287e867c
Trishna <trishna.patil13@gmail.com>
2024-08-11 19:40:49 -0400

    adding dice and buttons

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,4 +1,4 @@
-import 'package:first_app/styled_text.dart';
+//import 'package:first_app/styled_text.dart';
 import 'package:flutter/material.dart';
 
 const startAlignment = Alignment.topLeft;
@@ -10,6 +10,8 @@ class GradientContainer extends StatelessWidget {
 
   final List<Color> colors;
 
+  void rollDice() {}
+
   @override
   Widget build(context) {
     return Container(
@@ -20,8 +22,20 @@ class GradientContainer extends StatelessWidget {
           end: endAlignment,
         ),
       ),
-      child: const Center(
-        child: StyledText('Hello World!'),
+      child: Center(
+        child: Column(
+          children: [
+            Image.asset(
+              'assets/images/dice-images/dice-2.png',
+              width: 200,
+            ),
+            TextButton(
+              onPressed: rollDice,
+              child: const Text('Roll Dice'),
+            )
+          ],
+        ),
+        //StyledText('Hello World!'),
       ),
     );
   }
